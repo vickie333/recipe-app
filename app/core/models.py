7b6169a86ac2d1f58ddfb4d80fe2bf63f91742fc
vickie333 <mariavictoriaperezcontreras@gmail.com>
2025-10-28 17:31:04 -0400

    Implement user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    """Managers for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False) #LOGIN WITH DJANGO ADMIN
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
