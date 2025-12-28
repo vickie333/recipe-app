0ab58a57849b6ae2b1a4e833c77ec23038e50123
Maria Victoria Pérez Contrera <mariavictoriaperezcontreras@gmail.com>
2025-12-28 16:07:13 -0300

    feat: Allow recipe image to be updated via the recipe detail serializer.

--- apps/recipe/serializers.py
+++ apps/recipe/serializers.py
@@ -75,7 +75,6 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view"""
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description', 'image']
-        read_only_fields = RecipeSerializer.Meta.read_only_fields + ['image']
 
 class RecipeImageSerializer(serializers.ModelSerializer):
     """Serializer for uploading images to recipes"""
